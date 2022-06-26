b9a4f3f6d4511db534fad4ecfac88a537dc392a4
Andre Faustino <andre.domingues.faustino@gmail.com>
2022-06-25 22:10:38 -0300

    Config Server Done

---
+++ config-server/src/main/java/com/microservices/config/server/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.microservices.config.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/actuator/**")
+                .antMatchers("/encrypt/**")
+                .antMatchers("/decrypt/**");
+        super.configure(web);
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().antMatchers("/actuator/**", "/encrypt/**", "/decrypt/**")
+//                .permitAll().and().csrf().disable();
+//    }
+
+}
